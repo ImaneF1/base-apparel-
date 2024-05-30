32cb2cff100b7a06d2ee27aae6b01b1e768504ab
ImaneF1 <i.fouad@aui.ma>
2024-05-30 10:56:07 +0100

    Projet 1 base apparel

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import HeroSection from "@/components/HeroSection";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className="bg-gradient-to-tl from-whiteCleear to-whiteAlmost">
+      <HeroSection />
+    </main>
+  );
+}
